e004b6a45599299813535e87ba5360434de7906b
Zah1r07 <s300083@studenti.polito.it>
2026-06-11 01:42:30 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/' });
+const api = axios.create({ baseURL: 'https://walton-complete-project-backend.vercel.app/api/' || 'http://localhost:8000/api/' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken');
